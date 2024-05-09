1a77adff7f48c08b91ac4ea38cb4ed049bb71fba
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-05-09 10:24:47 +0700

    feat: Push Project and Unit Testing 🔥✨✅

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * @param {string[]} words
+ * @returns
+ */
+const groupAndSortLetters = (words) => {
+  let result = "";
+
+  let combinedString = words.join("");
+
+  let letterCounts = {};
+  for (let char of combinedString) {
+    if (letterCounts[char] === undefined) {
+      letterCounts[char] = 0;
+    }
+    letterCounts[char]++;
+  }
+
+  let sortedLetters = Object.entries(letterCounts).sort((a, b) => {
+    if (a[1] === b[1]) {
+      return b[0] < a[0] ? 1 : -1;
+    } else {
+      return b[1] - a[1];
+    }
+  });
+
+  for (let [char] of sortedLetters) {
+    result += char;
+  }
+
+  return result;
+};
+
+const words = ["Pendanaan", "Terproteksi", "Untuk", "Dampat", "Berarti"];
+const sortedLetters = groupAndSortLetters(words);
+console.log(sortedLetters);
+
+module.exports = groupAndSortLetters;
---
+++ src/index.test.spec.js
@@ -0,0 +1,48 @@
+const groupAndSortLetters = require("./index.js");
+
+describe("groupAndSortLetters", () => {
+  it("should return sorted and grouped letters with first example", () => {
+    const words = ["Abc", "bCd"];
+    const expectedOutput = "bACcd";
+    expect(groupAndSortLetters(words)).toEqual(expectedOutput);
+  });
+
+  it("should return sorted and grouped letters with second example", () => {
+    const words = ["Oke", "One"];
+    const expectedOutput = "Oekn";
+    expect(groupAndSortLetters(words)).toEqual(expectedOutput);
+  });
+
+  it("should return sorted and grouped letters", () => {
+    const words = ["Pendanaan", "Terproteksi", "Untuk", "Dampat", "Berarti"];
+    const expectedOutput = "aenrtikpBDPTUdmosu";
+    expect(groupAndSortLetters(words)).toEqual(expectedOutput);
+  });
+
+  it("should handle empty array input", () => {
+    const words = [];
+    expect(groupAndSortLetters(words)).toEqual("");
+  });
+
+  it("should handle array with empty strings", () => {
+    const words = ["", "", ""];
+    expect(groupAndSortLetters(words)).toEqual("");
+  });
+
+  it("should handle array with single-character words", () => {
+    const words = ["a", "b", "c", "d"];
+    expect(groupAndSortLetters(words)).toEqual("abcd");
+  });
+
+  it("should handle array with words containing special characters", () => {
+    const words = ["@abc", "$def", "!ghi"];
+    const expectedOutput = "!$@abcdefghi";
+    expect(groupAndSortLetters(words)).toEqual(expectedOutput);
+  });
+
+  it("should handle array with words containing non-alphabetic characters", () => {
+    const words = ["123abc", "456def", "789ghi"];
+    const expectedOutput = "123456789abcdefghi";
+    expect(groupAndSortLetters(words)).toEqual(expectedOutput);
+  });
+});
